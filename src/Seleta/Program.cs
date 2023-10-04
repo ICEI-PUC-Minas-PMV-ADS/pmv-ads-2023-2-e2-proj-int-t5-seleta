ac827af6f3ee70868d183eaeeda06613b7b868ac
Victor Bastos <vbastosmaciel@outlook.com.br>
2023-10-04 18:49:41 -0300

    feat: Criando conexão com banco de dados

--- src/Seleta/Program.cs
+++ src/Seleta/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Seleta.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opts => opts.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
