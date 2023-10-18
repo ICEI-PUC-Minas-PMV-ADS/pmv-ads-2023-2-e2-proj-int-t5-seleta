c321e66f8ae48b7b9b969ee7217160f4727529bf
Gabriela Penaforte <devgabrielapenaforte@gmail.com>
2023-10-18 18:56:00 -0300

    adição do menu mobile na página inicial e no perfil do usuário

--- src/Seleta/wwwroot/js/site.js
+++ src/Seleta/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
