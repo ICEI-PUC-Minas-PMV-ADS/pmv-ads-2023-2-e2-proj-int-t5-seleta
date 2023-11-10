9161dad7c5636f227affa371667278e8efeaf195
Gabriela Penaforte <devgabrielapenaforte@gmail.com>
2023-11-10 14:51:49 -0300

    adicionadas dicas de uso na página inicial

--- src/Seleta/wwwroot/js/site.js
+++ src/Seleta/wwwroot/js/site.js
@@ -1,8 +1,32 @@
-﻿function menuShow() {
+﻿
+// RESPONSIVIDADE
+function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
     } else {
         menuMobile.classList.add('open');
     }
+}
+
+// BOTÃO DE DICA
+
+function showConteudoCategoria() {
+    let conteudo = document.querySelector(".conteudo-categoria");
+    conteudo.style.display = "inline-grid";
+}
+
+function hideConteudoCategoria() {
+    let conteudo = document.querySelector(".conteudo-categoria");
+    conteudo.style.display = "none";
+}
+
+function showConteudoPesquisa() {
+    let conteudo = document.querySelector(".conteudo-pesquisa");
+    conteudo.style.display = "inline-grid";
+}
+
+function hideConteudoPesquisa() {
+    let conteudo = document.querySelector(".conteudo-pesquisa");
+    conteudo.style.display = "none";
 }
