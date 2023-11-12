9b61de4af5419861d3ef3e13fee92c2c875bff50
Victor Bastos <vbastosmaciel@outlook.com.br>
2023-11-12 16:14:57 -0300

    Ajustando gitignore

--- src/Seleta/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-    } else {
-        menuMobile.classList.add('open');
-    }
-}
