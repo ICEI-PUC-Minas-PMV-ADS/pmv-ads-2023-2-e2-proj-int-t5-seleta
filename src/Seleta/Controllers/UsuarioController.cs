9df19c9d8d368c00c445647b86bf53cca7ea490e
Victor Bastos <vbastosmaciel@outlook.com.br>
2023-11-13 17:57:12 -0300

    Ajustes details e delete

--- src/Seleta/Controllers/UsuarioController.cs
+++ src/Seleta/Controllers/UsuarioController.cs
@@ -13,6 +13,13 @@ namespace Seleta.Controllers
     {
         private readonly ApplicationDbContext _context;
 
+        /**
+         * Propriedade da classe para salvar o cpf do usuario
+         * Não foi inicializado ainda, só declarado, a gente passa um valor pra ele no método do perfil
+         * Ele é estático pra sempre que usuario mudar ele mudar também e não ocupar muita memoria
+         */
+        private static string? usuarioCpf;
+
         public UsuarioController(ApplicationDbContext context)
         {
             _context = context;
@@ -100,22 +107,25 @@ namespace Seleta.Controllers
         [Authorize]
         public async Task<IActionResult> Perfil()
         {
-            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var userLogged = await _context.Usuarios.FindAsync(userId);
+            //A variável é inicializada aqui, eu uso o HttpContext pra pegar o cpf do usuario logado
+            usuarioCpf =  HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userLogged = await _context.Usuarios.FindAsync(usuarioCpf);
             return View(userLogged);
         }
 
         //DETAILS
-
-        public async Task<IActionResult> Details(string? id)
+        [Authorize]
+        public async Task<IActionResult> Details()
         {
-            if (id == null) 
+            //Como o cpf é uma variavel da classe, eu consigo usar ela aqui
+            if (usuarioCpf == null) 
             {
                 return NotFound();
             }
 
+            //Aqui
             var usuario = await _context.Usuarios
-                .FirstOrDefaultAsync(m => m.CPF == id);
+                .FirstOrDefaultAsync(m => m.CPF == usuarioCpf);
 
             if (usuario == null) 
             {
@@ -126,16 +136,18 @@ namespace Seleta.Controllers
         }
 
         //DELETE
-
-        public async Task<IActionResult> Delete(string? id)
+        [Authorize]
+        public async Task<IActionResult> Delete()
         {
-            if (id == null)
+            //Aqui
+            if (usuarioCpf == null)
                { 
                 return NotFound();
                }
 
+            //E aqui, dai não precisa ficar pegando o cpf em todos os métodos :D
             var usuario = await _context.Usuarios
-                .FirstOrDefaultAsync(m => m.CPF == id);
+                .FirstOrDefaultAsync(m => m.CPF == usuarioCpf);
 
             if (usuario == null) 
             {
@@ -147,14 +159,20 @@ namespace Seleta.Controllers
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string? id)
+        public async Task<IActionResult> DeleteConfirmed()
         {
-            var usuario = await _context.Usuarios.FindAsync(id);
-             _context.Usuarios.Remove(usuario);
+            var usuario = await _context.Usuarios.FindAsync(usuarioCpf);
+            _context.Usuarios.Remove(usuario);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Login));
         }
 
+        /**
+         * Acho que nem precisa desse método, talvez fosse mais interessante se
+         * ele retornasse o usuario logado com o cpf
+         * Eu mudaria o codigo pra esse await _context.Usuarios.FirstOrDefaultAsync(m => m.CPF == usuarioCpf);
+         * E dai usaria essa função sempre que precisar retornar o usuario
+         */
         private bool UsuarioExists(string id)
         {
             return _context.Usuarios.Any(e => e.CPF == id);
